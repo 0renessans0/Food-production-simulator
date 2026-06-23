82cdef097b4dafae76b6ef341b0f4e059598f87e
0renessans0 <jimina056@gmail.com>
2026-06-23 15:01:54 +0300

    сборка может потом исправлю по мелочи

--- unity-game/Build/unity-game.loader.js
+++ unity-game/Build/unity-game.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58440108,
+    wasmFileSize: 58445306,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
